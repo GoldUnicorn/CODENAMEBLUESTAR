d29bd925df702231d52773f93d20d8ee6d0ec3b2
Ryder Saint <rydersaint@gmail.com>
2016-11-30 15:00:01 +0900

    Added jobscheduler
    A container to run scheduled cron tasks

--- api(s)/openexchangerates/getlastestrates.sh
+++ api(s)/openexchangerates/getlastestrates.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 #
 ## Get lastest currency rates from openexchangerates.org
+# parse the json and remap for table
+# Then import them into the ddb
+# Varibles
+# fs_path - where are the files stored
+fs_path=""
+# api command
+api_command="https://openexchangerates.org/api/latest.json?app_id=1635c3d9ef814ad6b0fa4b778d2a0e9e&base=USD"
 #
-#
-#
-#
+# Get the lastest currency data
+curl $api_command > $fs_pathcurrency.json
---
+++ servers/pipes/jobscheduler/readme.md
@@ -0,0 +1,16 @@
+# jobscheduler
+
+A container to run scheduled cron tasks
+
+installed
+
+- curl
+- wget
+- sudo
+- crond
+- zlib1g-dev
+- pip
+- supervisor
+- nodejs and npm
+- crontab-ui
+- jq
